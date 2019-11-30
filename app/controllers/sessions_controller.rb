cd2e24a46e5a4709a433bf269568fe420e500d60
Aurangzaib <aurangzaib.danial@gmail.com>
2019-11-30 23:54:12 +0500

    basic signup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,13 @@
 class SessionsController < ApplicationController
   get '/signup' do
     @title = 'Signup!'
+
     erb :'sessions/signup.html'
   end
 
-  
+
+  post '/signup' do
+    raise params.inspect
+  end
+
 end
