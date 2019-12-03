632e8151c0923bf2d71a475fa5d6430bd29d8a59
Aurangzaib <aurangzaib.danial@gmail.com>
2019-12-03 05:36:51 +0500

    added information on home page and footer content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,9 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:user_id]
     end
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
   end
 end
