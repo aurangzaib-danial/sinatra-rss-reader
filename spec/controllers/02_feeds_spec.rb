dc5ab74a18891434ce7d360a3ade0a1f0dd7eced
Aurangzaib <aurangzaib.danial@gmail.com>
2019-12-05 00:50:37 +0500

    add tests for a specific feed and article

--- spec/controllers/02_feeds_spec.rb
+++ spec/controllers/02_feeds_spec.rb
@@ -24,6 +24,43 @@ describe 'Feeds' do
     expect(last_response.body).to include('About')
   end
 
-  
+  it 'a feed articles cannot be accessed if it does not belong to the logged in user' do
+
+    params = {
+      email: 'aurangzaib.danial@gmail.com', 
+      password: 'khan1234'
+    }
+    user_1 = User.create(params)
+    user_2 = User.create(email: 'sunny@example.com', password: 'mypass12345')
+
+    post '/login', params
+
+    feed = Feed.create(title: 'Test Feed', user: user_2)
+
+    get "/feeds/#{feed.id}/articles"
+    follow_redirect!
+
+    expect(last_response.body).to include('Feeds')
+
+  end
+
+  it 'an article of a feed cannot be accessed if the feed does not belong to the user' do
+    params = {
+      email: 'aurangzaib.danial@gmail.com', 
+      password: 'khan1234'
+    }
+    user_1 = User.create(params)
+    user_2 = User.create(email: 'sunny@example.com', password: 'mypass12345')
+
+    post '/login', params
+
+    feed = Feed.create(title: 'Test Feed', user: user_2)
+    article = Article.create(feed: feed)
+
+    get "/feeds/#{feed.id}/articles/#{article.id}"
+    follow_redirect!
+
+    expect(last_response.body).to include('Feeds')
+  end
 
 end
