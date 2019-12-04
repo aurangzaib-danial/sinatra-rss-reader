403739f781e56cb47355ceaf6202ad4978fa45f4
Aurangzaib <aurangzaib.danial@gmail.com>
2019-12-04 22:18:24 +0500

    rename feeds controll spec file and moved login and signup specs into one sessions controller spec

--- spec/controllers/01_sessions_spec.rb
+++ spec/controllers/01_sessions_spec.rb
@@ -1,3 +1,31 @@
+describe 'Login' do
+  it 'login form on submit, successfully logs in' do
+    params = {
+      email: 'aurangzaib.danial@gmail.com', 
+      password: 'khan1234'
+    }
+    User.create(params)
+    
+    post '/login', params
+
+    expect(last_response.location).to include('/')
+  end
+
+  it 'cannot visit /login if already logged in' do
+    params = {
+      email: 'aurangzaib.danial@gmail.com', 
+      password: 'khan1234'
+    }
+    User.create(params)
+    
+    post '/login', params
+
+    get '/login'
+
+    expect(last_response.location).to include('/')
+  end
+end
+
 describe 'Signup' do
   it 'signup form on submit creates the user and redirects to root' do
     params = {
